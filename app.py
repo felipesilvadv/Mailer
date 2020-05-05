cecd4f7f976f7691c5aa435f9fb22432a2514f51
felipesilvadv <lfsilva1@uc.cl>
2020-05-05 03:27:48 -0400

    Adding a multiple recipient

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from src.mailer import sendMail
 
-to = "example@example.org"
+to = "example@example.org, otherexample@example.org"
 asunto = "Prueba"
 mensaje = "Hola te estoy mandando un email"
 sendMail(to, asunto, mensaje)
